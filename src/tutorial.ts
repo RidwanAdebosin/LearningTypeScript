6591a94a21fb07be17b67eb53f1f9c94c143a899
RidwanAdebosin <adebosinadewale@gmail.com>
2024-05-08 13:35:51 +0100

    learning typecript

---
+++ src/tutorial.ts
@@ -0,0 +1,12 @@
+console.log("TypeScript Tutorial")
+
+interface someValue {
+    name: string;
+    id: number;
+}
+
+let someObj: someValue = {
+    name: "Ridwam",
+    id: 123,
+}
+console.log(someObj);
