ffadaf77fbe30fb20ecdd3b306e6a0a9d4b48582
RidwanAdebosin <adebosinadewale@gmail.com>
2024-05-19 21:17:11 +0100

    literal typescript

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -20,5 +20,39 @@ let amount:number = 20;
 amount= 12 - 1;
 // amount = "pants"
 
-let isAwesome
+let isAwesome:boolean =  true;
+isAwesome= false;
+// isAwesome = "shakeAndbake";
+
+// typescript union
+let tax: number | string = 10;
+tax = 10;
+tax = "$10"
+
+// literal value
+let requestStatus:"pending" | "success" | "error" = "pending";
+requestStatus = "success";
+// requestStatus = "random";
+
+let notSure:any = 4;
+notSure = "maybe a string";
+notSure = false;
+
+let random:number;
+
+const books = ["1984", "Brave New World", "Fahrenheit 451"];
+let foundBook:string | undefined;
+
+for (let book of books) {
+    if (book === "1984") {
+        foundBook = book;
+        foundBook = foundBook.toUpperCase()
+        break;
+    }
+}
+
+foundBook?.length
+
+
+
 
