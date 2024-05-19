d9fcef27ffa5697f7a0c157a1565a1c6a3310949
RidwanAdebosin <adebosinadewale@gmail.com>
2024-05-19 20:50:10 +0100

    type annotation

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -10,3 +10,15 @@ let someObj: someValue = {
     id: 123,
 }
 console.log(someObj);
+
+let awesomeName:string = "shakeAndBake"
+awesomeName = awesomeName.toUpperCase();
+awesomeName
+// awesomeName = 20;
+
+let amount:number = 20;
+amount= 12 - 1;
+// amount = "pants"
+
+let isAwesome
+
