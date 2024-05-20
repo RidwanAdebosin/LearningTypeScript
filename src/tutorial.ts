408dec0155e913d787a11740f6b0d41aab7d4c96
RidwanAdebosin <adebosinadewale@gmail.com>
2024-05-20 09:08:11 +0100

    Objects in TypeScript

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -99,3 +99,15 @@ let notebook = {title: 'notebook'}
 
 let items: { readonly title:string; cost?: number }[] = [book, pen, notebook];
 // items[0].title = 'new book';
+
+let bike:{brand:string; year:number} = {brand: 'yamaha', year: 2010};
+// bike.year = 'old'
+let laptop:{brand:string; year:number} = {brand: 'Dell', year: 2020};
+// let laptop2:{brand:string; year:number} = {brand: 'HP'};
+
+let product1 = {title: 'shirt', price:20};
+let product2 = {title: 'pants'};
+let products: {title:string; price?: number}[] = [product1, product2];
+
+// products.push({title: "shoes", price: 'expensive'})
+
